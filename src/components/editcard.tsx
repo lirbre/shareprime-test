cdb4c99f2b44a8cf1ede7adb6d74990e6d44a3c1
Breno Lira <brenoliradev@gmail.com>
2022-08-16 18:35:41 -0300

    chore: mobile card

--- src/components/editcard.tsx
+++ src/components/editcard.tsx
@@ -7,11 +7,13 @@ export const EditCard = ({ image, title, id }: ItemProps) => {
   return (
     <div
       onClick={() => router.push(`/admin/edit/${id}`)}
-      className="flex h-[90px] w-full cursor-pointer items-center justify-between border border-[#000]/20 p-2.5 shadow-md shadow-[#000]/20"
+      className="flex h-full w-full flex-col items-center justify-between gap-4 border border-[#000]/20 p-2.5 shadow-md shadow-[#000]/20 md:h-[90px] md:flex-row md:gap-0"
     >
       {/* next image would need hostname config */}
       <img width={75} height={75} alt={`${title} - Image`} src={image} />
-      <small className="mt-auto flex w-2/3 justify-end">{title}</small>
+      <small className="mt-auto flex w-2/3 justify-center md:justify-end">
+        {title}
+      </small>
     </div>
   )
 }
--- src/components/itemcard.tsx
+++ src/components/itemcard.tsx
@@ -3,10 +3,12 @@ import { ItemProps } from '@/typings/items'
 export const ItemCard = ({ image, link, title }: ItemProps) => {
   return (
     <a href={link} target={'_blank'} rel="noreferrer">
-      <div className="flex h-[90px] w-full items-center justify-between border border-[#000]/20 p-2.5 shadow-md shadow-[#000]/20">
+      <div className="flex h-full w-full flex-col items-center justify-between gap-4 border border-[#000]/20 p-2.5 shadow-md shadow-[#000]/20 md:h-[90px] md:flex-row md:gap-0">
         {/* next image would need hostname config */}
         <img width={75} height={75} alt={`${title} - Image`} src={image} />
-        <small className="mt-auto flex w-2/3 justify-end">{title}</small>
+        <small className="mt-auto flex w-2/3 justify-center md:justify-end">
+          {title}
+        </small>
       </div>
     </a>
   )
