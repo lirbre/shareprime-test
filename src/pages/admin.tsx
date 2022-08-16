f0d1bc709876d69667ad84791c8b51a1486ad510
Breno Lira <brenoliradev@gmail.com>
2022-08-16 13:14:32 -0300

    chore[pages]: start /admin page

---
+++ src/pages/admin.tsx
@@ -0,0 +1,41 @@
+import { Meta } from '@/layouts'
+import { Main } from '@/templates'
+
+const Admin = () => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="SharePrime - Admin"
+          description="Admin page using Fluid UI"
+        />
+      }
+    >
+      <div className="container">
+        <h4>Sphinx of black quartz, judge my vow.</h4>
+        <h5>Sphinx of black quartz, judge my vow.</h5>
+        <h6>Sphinx of black quartz, judge my vow.</h6>
+        <p>
+          Sphinx of black quartz, judge my vow. Sphinx of black quartz, judge my
+          vow.Sphinx of black quartz, judge my vow. Sphinx of black quartz,
+          judge my vow. Sphinx of black quartz, judge my vow. Sphinx of black
+          quartz, judge my vow.
+        </p>
+        <p>
+          Sphinx of black quartz, judge my vow. Sphinx of black quartz, judge my
+          vow.Sphinx of black quartz, judge my vow. Sphinx of black quartz,
+          judge my vow. Sphinx of black quartz, judge my vow. Sphinx of black
+          quartz, judge my vow.
+        </p>
+        <p>
+          Sphinx of black quartz, judge my vow. Sphinx of black quartz, judge my
+          vow.Sphinx of black quartz, judge my vow. Sphinx of black quartz,
+          judge my vow. Sphinx of black quartz, judge my vow. Sphinx of black
+          quartz, judge my vow.
+        </p>
+      </div>
+    </Main>
+  )
+}
+
+export default Admin
