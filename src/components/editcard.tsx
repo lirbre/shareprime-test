e2874c36b5a68168a19feaee4eda897983c90aaf
Breno Lira <brenoliradev@gmail.com>
2022-08-17 11:37:58 -0300

    chore: cursor-pointer on edit cards to make UX better

--- src/components/editcard.tsx
+++ src/components/editcard.tsx
@@ -7,7 +7,7 @@ export const EditCard = ({ image, title, id }: ItemProps) => {
   return (
     <div
       onClick={() => router.push(`/admin/edit/${id}`)}
-      className="flex h-full w-full flex-col items-center justify-between gap-4 border border-[#000]/20 p-2.5 shadow-md shadow-[#000]/20 md:h-[90px] md:flex-row md:gap-0"
+      className="flex h-full w-full cursor-pointer flex-col items-center justify-between gap-4 border border-[#000]/20 p-2.5 shadow-md shadow-[#000]/20 md:h-[90px] md:flex-row md:gap-0"
     >
       {/* next image would need hostname config */}
       <img width={75} height={75} alt={`${title} - Image`} src={image} />
